0f5a18260463112dc19ddf52e69006b841f3bc40
Axel <axel.o.nilsson@gmail.com>
2022-04-25 02:13:36 +0200

    Refactor and comments, gave part 1 of SonarSweep a separate method

--- 2021/Day 1 - SonarSweep/SonarSweep.rb
+++ 2021/Day 1 - SonarSweep/SonarSweep.rb
@@ -23,19 +23,31 @@ def amountOfSlidingWindowIncreases(array)
 
 end
 
+# Part 1
+def slidingWindowIncreasesFromInput
+
+    # AoC Puzzle Input from file
+    path = __dir__ + "./PuzzleInput.txt"
+    array = File.read(path).split.map(&:to_i)
+
+    return amountOfSlidingWindowIncreases(array)
+
+end
+
+# Part 2 
 def sumsOfThreeDepths
     
-    # AoC Puzzle Key
+    # AoC Puzzle Input from file
     path = __dir__ + "./PuzzleInput.txt"
-    data = File.read(path).split.map(&:to_i)
+    array = File.read(path).split.map(&:to_i)
 
-    size = data.size
+    size = array.size
 
     sums = []
 
     for index in 0..(size-1) do
         unless (index >= size - 2)
-            sum = data[index] + data[index + 1] + data[index + 2]
+            sum = array[index] + array[index + 1] + array[index + 2]
             sums.push(sum)
         end
     end
@@ -44,5 +56,8 @@ def sumsOfThreeDepths
 
 end
 
-# Print amount of times the sum of 3 depths increased
+# Part 1 - Print amount of times the depth increased compared to previous depth
+puts slidingWindowIncreasesFromInput
+
+# Part 2 - Print amount of times the sum of 3 following depths increased compared to previous sum of 3 depths
 puts sumsOfThreeDepths
