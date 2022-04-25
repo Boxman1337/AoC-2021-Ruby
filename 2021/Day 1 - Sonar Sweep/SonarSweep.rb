6770092cf1e9928794bdf3ba73fba792291c2b5e
Axel <axel.o.nilsson@gmail.com>
2022-04-25 02:18:14 +0200

    Code tidying

--- 2021/Day 1 - Sonar Sweep/SonarSweep.rb
+++ 2021/Day 1 - Sonar Sweep/SonarSweep.rb
@@ -9,34 +9,27 @@ def amountOfSlidingWindowIncreases(array)
     # For each depth: compare with previous depth
     # If current depth is greater than previous depth, increment counter and save current depth  
     array.each do |currentDepth|
-
         if ((currentDepth > previousDepth) and (previousDepth != 0)) 
             depthIncreasedFromPrev = depthIncreasedFromPrev + 1 
         end
-        
         previousDepth = currentDepth
-
     end 
 
     # Return amount of times the depth increased
     return depthIncreasedFromPrev
-
 end
 
 # Part 1
 def slidingWindowIncreasesFromInput
-
     # AoC Puzzle Input from file
     path = __dir__ + "./PuzzleInput.txt"
     array = File.read(path).split.map(&:to_i)
-
+    
     return amountOfSlidingWindowIncreases(array)
-
 end
 
 # Part 2 
 def sumsOfThreeDepths
-    
     # AoC Puzzle Input from file
     path = __dir__ + "./PuzzleInput.txt"
     array = File.read(path).split.map(&:to_i)
@@ -53,7 +46,6 @@ def sumsOfThreeDepths
     end
 
     return amountOfSlidingWindowIncreases(sums)
-
 end
 
 # Part 1 - Print amount of times the depth increased compared to previous depth
